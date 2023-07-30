4c458b27839783c9a9329590c6f637bbbf9f2370
Matheus Ferreira <matheusfersilva.10@gmail.com>
2023-07-30 18:27:26 -0300

    update exams endpoint

--- exams-app/server.rb
+++ exams-app/server.rb
@@ -5,13 +5,41 @@ require 'pg'
 
 conn = PG::Connection.new('localhost', 5432, nil, nil, 'rebase-labs', 'postgres', 'password')
 
-get '/tests' do
-  result = conn.exec('SELECT * FROM medicalexams LIMIT 5')
-  arr = []
-  result.each do |tuple|
-    arr << tuple.to_h
+def get_doctor_by_crm(conn, crm)
+  result = conn.exec_params('SELECT * FROM doctors where crm = $1::text', [crm])
+  result[0]
+end
+
+def get_all_tests_by_result_token(conn, token)
+  result = conn.exec_params('SELECT exam_type, exam_type_limits, exam_type_result from medicalexams where exam_result_token = $1::text', [token])
+  (0..result.ntuples - 1).map { |n| result[n] }
+end
+
+get '/exams' do
+  limit = params['limit'].to_i
+  page = params['page'].to_i
+  offset = (page - 1) * limit
+  has_next = false
+
+  exams = conn.exec_params("SELECT DISTINCT
+    exam_result_token, exam_date, patient_cpf, patients.name, patients.email, patients.birthdate, doctor_crm
+    FROM medicalexams
+    INNER JOIN patients ON cpf = patient_cpf LIMIT $1::int OFFSET $2::int", [(limit + 1), offset])
+
+  if (exams.ntuples > limit)
+    has_next = true
   end
-  arr.to_json
+
+  exam_list = (0..limit - 1).map do |n|
+    exam = exams[n]
+    tests = get_all_tests_by_result_token(conn, exam['exam_result_token'])
+    doctor = get_doctor_by_crm(conn, exam['doctor_crm'])
+    exam['doctor'] = doctor
+    exam['tests'] = tests
+    exam
+  end
+
+  { data: exam_list, has_next: has_next}.to_json
 end
 
 get '/' do
